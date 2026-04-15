52ff9528fe8e96e2ee0507b59e875541f97d4710
cchowgule <cchowgule@gmail.com>
2026-04-15 23:36:41 +0530

    Add rainbow-csv

---
+++ lua/custom/plugins/rainbox-csv.lua
@@ -0,0 +1,14 @@
+return {
+  'cameron-wags/rainbow_csv.nvim',
+  config = true,
+  ft = { 'csv', 'tsv', 'csv_semicolon', 'csv_whitespace', 'csv_pipe', 'rfc_csv', 'rfc_semicolon' },
+  cmd = {
+    'RainbowDelim',
+    'RainbowDelimSimple',
+    'RainbowMultiDelim',
+    'NoRainbowDelim',
+    'RainbowQuery',
+    'RainbowAlign',
+    'RainbowShrink',
+  },
+}
